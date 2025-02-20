86f11c5f0132e975b48998623dcc1817cb7039eb
Leka Uïla <liam.flandrinck.58@gmail.com>
2025-02-20 20:02:21 +0100

    adding maintest

---
+++ Module00/mainFunctions.ml
@@ -0,0 +1,51 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   mainFunctions.ml                                   :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2025/02/20 19:28:17 by lflandri          #+#    #+#             *)
+(*   Updated: 2025/02/20 20:00:52 by lflandri         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+(* EX00 main test *)
+
+let main () : unit =
+	print_endline("Test pour 42 :");
+	ft_test_sign(42);
+	print_endline("Test pour 0 :");
+	ft_test_sign(0);
+	print_endline("Test pour -42 :");
+	ft_test_sign(-42)
+
+let x = main()
+
+
+
+(* EX01 main test *)
+
+let main () : unit =
+	print_endline("Test pour 3 :");
+	ft_countdown(3);
+	print_endline("Test pour 0 :");
+	ft_countdown(0);
+	print_endline("Test pour -1 :");
+	ft_countdown(-1)
+
+let x = main()
+
+
+
+(* EX02 main test *)
+
+let main () : unit =
+	print_endline("Test pour 2 4 :");
+	ft_power (2,4 );
+	print_endline("Test pour 3 0 :");
+	ft_power (3, 0);
+	print_endline("Test pour 0 5 :");
+	ft_power (0, 5)
+
+let x = main()
