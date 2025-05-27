d6d16320b4c9be7415b7508aece7f2e5133068ad
Liam Flandrinck <lflandri@3E2.42angouleme.fr>
2025-05-27 13:11:40 +0200

    M04E01

--- Module04/ex01/ft_ref.ml
+++ Module04/ex01/ft_ref.ml
@@ -6,30 +6,47 @@
 (*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/04/24 15:27:34 by lflandri          #+#    #+#             *)
-(*   Updated: 2025/04/24 16:55:46 by lflandri         ###   ########.fr       *)
+(*   Updated: 2025/05/27 13:11:04 by lflandri         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-type 'a ft_ref = {content: 'a}
+type 'a ft_ref = {mutable content: 'a}
 
 
-let creacontent l : 'a ft_ref  = 
+let return l : 'a ft_ref  = 
   {content= l}
 
 let printcontent l : unit = 
-  let {content} = l in
-    print_int content
+    print_int l.content;
+    print_char '\n'
 
-let setcontent l a : unit =
-  let {content} = l in
-    let content = a in ()
+let set (l:'a ft_ref)(a:'a) : unit =
+  l.content <- a
 
-let x = creacontent 5;;
-let y = x;;
-printcontent x;;
-printcontent y;;
-let x = setcontent x, 3;;
-printcontent x;;
-printcontent y;;
+let get l : 'a =
+  l.content
 
+let bind (l:'a ft_ref)(func:('a -> 'b ft_ref)) : 'b ft_ref =
+  func l.content
+
+let testbindfunc (a:'a) : 'b ft_ref =
+  {content= string_of_int a}
+
+let main () : unit =
+  let x = return 5 in
+  let y = x in
+  printcontent x;
+  printcontent y;
+  set x 3;
+  printcontent x;
+  printcontent y;
+  let t = bind x testbindfunc in
+  print_string (get t);
+  print_char '\n';
+  set t "42 is life !";
+  print_string (get t);
+  print_char '\n'
+
+
+let x = main ()
 
