aa295688aff3f0df54798d1aacf577dd86aec45c
Leka Uïla <liam.flandrinck.58@gmail.com>
2025-09-09 12:02:22 +0200

    adding tardis ascii art

--- Module06/ex01/doctor.ml
+++ Module06/ex01/doctor.ml
@@ -6,7 +6,7 @@
 (*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/09/09 11:11:45 by lflandri          #+#    #+#             *)
-(*   Updated: 2025/09/09 11:34:43 by lflandri         ###   ########.fr       *)
+(*   Updated: 2025/09/09 12:01:21 by lflandri         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -23,7 +23,26 @@ class doctor =
     method talk = print_endline ("Hi! I'm the Doctor!")
     method private regenerate = hp <- 100
     method use_sonic_screwdrive = print_endline "Whiiiiwhiiiwhiii Whiiiiwhiiiwhiii Whiiiiwhiiiwhiii"
-    method travel_in_time (d1: int) (d2: int) = age <- (d2 - d1 + age)
+    method travel_in_time (d1: int) (d2: int) = age <- (d2 - d1 + age);
+   print_endline "        ___";
+   print_endline "        | |";
+   print_endline "        | |";
+   print_endline "-------------------";
+   print_endline "-------------------";
+   print_endline " |  ___  |  ___  |";
+   print_endline " | | | | | | | | |";
+   print_endline " | |-+-| | |-+-| |";
+   print_endline " | |_|_| | |_|_| |";
+   print_endline " |  ___  |  ___  |";
+   print_endline " | |   | | |   | |";
+   print_endline " | |   | | |   | |";
+   print_endline " | |___| | |___| |";
+   print_endline " |  ___  |  ___  |";
+   print_endline " | |   | | |   | |";
+   print_endline " | |   | | |   | |";
+   print_endline " | |___| | |___| |";
+   print_endline " |       |       |";
+   print_endline "==================="
     method use_private_regenerate = self#regenerate
     method set_hp_to_50 = hp <- 50
     initializer print_string "Doctor "; print_string name; print_endline " created."
