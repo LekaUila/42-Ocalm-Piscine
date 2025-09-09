1aba9304a76c8d286796e738893d7303c698f372
Leka Uïla <liam.flandrinck.58@gmail.com>
2025-09-09 11:09:54 +0200

    M06E00

--- Module06/ex00/main.ml
+++ Module06/ex00/main.ml
@@ -6,10 +6,20 @@
 (*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/09/01 15:05:10 by lflandri          #+#    #+#             *)
-(*   Updated: 2025/09/09 10:20:36 by lflandri         ###   ########.fr       *)
+(*   Updated: 2025/09/09 11:04:23 by lflandri         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-include "people.ml"
+include People
 
-let p = people "Clara"
+
+
+let main () =
+  (* print_endline "test" *)
+  let p = new people "Clara" in
+  p#talk;
+  print_endline p#to_string;
+  p#die
+
+
+let () = main ()
--- Module06/ex00/people.ml
+++ Module06/ex00/people.ml
@@ -6,7 +6,7 @@
 (*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/09/01 15:05:10 by lflandri          #+#    #+#             *)
-(*   Updated: 2025/09/09 10:18:20 by lflandri         ###   ########.fr       *)
+(*   Updated: 2025/09/09 10:45:09 by lflandri         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -18,5 +18,5 @@ class people (n:string) =
     method to_string = "class people : (name=" ^ name ^ ", hp=" ^ (string_of_int hp) ^ ")"
     method talk = print_endline ("I'm " ^ name ^ "! Do you know the doctor?")
     method die = print_endline "Aaaarghh!"
-    initializer print_string "Peaople "; print_string self#name, print_endline " created."
+    initializer print_string "People "; print_string name; print_endline " created."
   end
