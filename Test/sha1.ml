31884168aca486d2966e7415feaa0e5614052da5
LekaUila <liam.flandrinck.58@gmail.com>
2025-12-01 11:31:38 +0100

    hash function donne

--- Test/sha1.ml
+++ Test/sha1.ml
@@ -6,19 +6,29 @@
 (*   By: Leka Uïla <liam.flandrinck.58@gmail.com    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/08/29 12:16:32 by lflandri          #+#    #+#             *)
-(*   Updated: 2025/11/27 17:48:58 by Leka Uïla        ###   ########.fr       *)
+(*   Updated: 2025/12/01 11:30:11 by Leka Uïla        ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
+
+(*TEST STRING : *)
+
+
+let teststring = "duck"
+
+(*
+let teststring = "42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test"
+*)
+
+
 (* https://www.youtube.com/watch?v=kmHojGMUn0Q *)
-(* continue at step 11 *)
 
 (*
 Step 1 : convert text into array of ascii code
 step 2 : convert to binary
 step 3 : make it 8 bits long by staking 0 in front
 step 4 : join all binary code et add a one at end
-step 5 : fill the end with zeo until lenght = 512 mod 448
+step 5 : fill the end with zeo until lenght = something
 step 6 : take binary join lenth from step 3, convert lenth in binary
 step 7 : add zero in front of lenth converted in binary until lenth of binary = 64
 step 8 : append step 7 to end of step 5
@@ -30,6 +40,80 @@ step 13 : use bitwise and variables assignement to do some shit
 step 14 : convert variables into hexadecimal
 step 15 : join them
 *)
+(*==================================================BINAY OPERATOR==============================================*)
+
+let rec xOR l1 l2 : int list =
+  match l1 with
+  | [] -> []
+  | hd1 :: tl1 ->
+    match l2 with
+    | [] -> []
+    | hd2 :: tl2 -> ((hd2 + hd1) mod 2) :: (xOR tl1 tl2)
+
+let rec _AND l1 l2 : int list =
+  match l1 with
+  | [] -> []
+  | hd1 :: tl1 ->
+    match l2 with
+    | [] -> []
+    | hd2 :: tl2 -> (hd2 * hd1) :: (_AND tl1 tl2)
+
+let rec _OR l1 l2 : int list =
+  match l1 with
+  | [] -> []
+  | hd1 :: tl1 ->
+    match l2 with
+    | [] -> []
+    | hd2 :: tl2 -> ((hd2 * hd1) + ((hd2 + hd1) mod 2)) :: (_OR tl1 tl2)
+
+let rec not l : int list =
+    match l with
+    | [] -> []
+    | hd :: tl -> ((hd + 1) mod 2) :: not tl
+
+let binaryAddition l1 l2 : int list =
+  let rec reverseList arg ret : int list =
+    match arg with
+    | [] -> ret
+    | hd :: tl -> reverseList tl (hd :: ret)
+  in
+  let rec binaryAdditionIntern l1 l2 toAdd : int list  =
+    let endCase l1 l2 toAdd = 
+      match l2 with
+      | [] -> [toAdd]
+      | hd1 :: tl1 -> binaryAdditionIntern l2 l1 toAdd
+    in
+      match l1 with
+      | [] -> endCase l1 l2 toAdd
+      | hd1 :: tl1 ->
+        match l2 with
+        | [] -> ((hd1 + toAdd) mod 2) :: (binaryAdditionIntern tl1 l2 ((hd1 + toAdd) / 2))
+        | hd2 :: tl2 -> 
+          ((hd2 + hd1 + toAdd) mod 2) :: (binaryAdditionIntern tl1 tl2 ((hd2 + hd1 + toAdd) / 2))
+          
+          (*let newl, toAdd = binaryAdditionIntern tl1 tl2 in
+          (((hd2 + hd1 + toAdd) mod 2) :: newl, if hd2 * hd1 + toAdd * hd1 + hd2 * toAdd >= 1 then 1 else 0)*)
+  in
+    let revl1 = reverseList l1 [] in
+    let revl2 = reverseList l2 [] in
+    let newL = binaryAdditionIntern revl1 revl2 0 in
+    reverseList newL []
+
+let rec truncate l nb : int list =
+  match l with
+  | [] -> []
+  | hd :: tl -> if nb <= 0 then [] else hd :: truncate tl (nb - 1)
+
+
+(*==================================================MINOR FUNCTION==============================================*)
+
+
+
+
+
+
+
+
 
 let charToBinaryString c : (int list) = 
   (*convert a char to a int list*)
@@ -201,14 +285,135 @@ let rec splitBlockIntoBlockOf nb l =
   | [] -> []
   | hd :: tl -> (splitIntoBlockOf nb hd) :: (splitBlockIntoBlockOf nb tl)
 
-
-let rec xOR l1 l2 =
-  match l1 with
-  | [] -> []
-  | hd1 :: tl1 ->
-    match l2 with
+let rec leftRotate l nb =
+  let leftRotateIntern l =
+    match l with
     | [] -> []
-    | hd2 :: tl2 -> ((hd2 + hd1) mod 2) :: (xOR tl1 tl2)
+    | hd :: tl -> concatList tl [hd]
+  in
+  if nb >= 1 then
+    leftRotate (leftRotateIntern l) (nb - 1)
+  else
+    l
+    
+
+let rec getBlockFromIndex (l:'a list) nb =
+  if nb <= 0 then
+    (
+    match l with
+      | [] -> []
+      | hd :: tl -> hd
+    )
+  else
+    (
+    match l with
+      | [] -> []
+      | hd :: tl -> getBlockFromIndex tl (nb - 1)
+    )
+
+
+let rec createNewBlocks (l:'a list) nb =
+  if nb >= 80 then
+    l
+  else
+    let wordA = getBlockFromIndex l (nb - 3) in
+    let wordB = getBlockFromIndex l (nb - 8) in
+    let wordC = getBlockFromIndex l (nb - 14) in
+    let wordD = getBlockFromIndex l (nb - 16) in
+    let xorA = xOR wordA wordB in
+    let xorB = xOR wordC xorA in
+    let xorC = xOR wordD xorB in
+    createNewBlocks (concatList l [leftRotate xorC 1]) (nb + 1)
+
+
+let rec extendChunks (l:'a list) =
+  match l with
+  | [] -> []
+  | hd :: tl ->
+    (createNewBlocks hd 16) :: extendChunks tl
+
+let rec chunkBitwiseOperation l a b c d e j : int list * int list * int list * int list * int list  =
+  if j < 20 then
+    (
+      let bandC = _AND b c in
+      let notB = _AND (not b) d in
+      let f = _OR bandC notB in
+      let k = [0;1;0;1;1;0;1;0;1;0;0;0;0;0;1;0;0;1;1;1;1;0;0;1;1;0;0;1;1;0;0;1] in
+
+      let word = getBlockFromIndex l j in
+      let tempA = binaryAddition (leftRotate a 5) f in
+      let tempB = binaryAddition tempA e in
+      let tempC = binaryAddition tempB k in
+      let temp = truncate (binaryAddition tempC word) 32 in
+      chunkBitwiseOperation l temp a (leftRotate b 30) c d (j + 1)
+    )
+  else
+    (
+      if j < 40 then
+        (
+          let bxorC = xOR b c in
+          let f = xOR bxorC d in
+          let k = [0;1;1;0;1;1;1;0;1;1;0;1;1;0;0;1;1;1;1;0;1;0;1;1;1;0;1;0;0;0;0;1] in
+
+          let word = getBlockFromIndex l j in
+          let tempA = binaryAddition (leftRotate a 5) f in
+          let tempB = binaryAddition tempA e in
+          let tempC = binaryAddition tempB k in
+          let temp = truncate (binaryAddition tempC word) 32 in
+          chunkBitwiseOperation l temp a (leftRotate b 30) c d (j + 1)
+        )
+      else
+        (
+          if j < 60 then
+            (
+              let bandC = _AND b c in
+              let bandD = _AND b d in
+              let candD = _AND c d in
+              let bandCorbandD = _OR bandC bandD in
+              let f = _OR bandCorbandD candD in
+              let k = [1;0;0;0;1;1;1;1;0;0;0;1;1;0;1;1;1;0;1;1;1;1;0;0;1;1;0;1;1;1;0;0] in
+
+              let word = getBlockFromIndex l j in
+              let tempA = binaryAddition (leftRotate a 5) f in
+              let tempB = binaryAddition tempA e in 
+              let tempC = binaryAddition tempB k in 
+              let temp = truncate (binaryAddition tempC word) 32 in
+              chunkBitwiseOperation l temp a (leftRotate b 30) c d (j + 1)
+            )
+          else
+            (
+              if j < 60 then
+                (
+                  let bxorC = xOR b c in
+                  let f = xOR bxorC d in
+                  let k = [1;1;0;0;1;0;1;0;0;1;1;0;0;0;1;0;1;1;0;0;0;0;0;1;1;1;0;1;0;1;1;0] in
+
+                  let word = getBlockFromIndex l j in
+                  let tempA = binaryAddition (leftRotate a 5) f in
+                  let tempB = binaryAddition tempA e in 
+                  let tempC = binaryAddition tempB k in 
+                  let temp = truncate (binaryAddition tempC word) 32 in
+                  chunkBitwiseOperation l temp a (leftRotate b 30) c d (j + 1)
+                )
+              else a, b, c, d, e
+                
+            )
+        )
+    )
+
+let rec chunksBitwiseOperation (l:'a list) h0 h1 h2 h3 h4 a b c d e : int list * int list * int list * int list * int list =
+  match l with
+  | [] -> h0, h1, h2, h3, h4
+  | hd :: tl -> let a, b, c, d, e = chunkBitwiseOperation hd a b c d e 0 in
+    chunksBitwiseOperation  tl
+                            (truncate (binaryAddition h0 a) 32)
+                            (truncate (binaryAddition h1 b) 32)
+                            (truncate (binaryAddition h2 c) 32)
+                            (truncate (binaryAddition h3 d) 32)
+                            (truncate (binaryAddition h4 e) 32)
+                            a b c d e
+
+(*==================================================PRINT FUNCTION==============================================*)
 
 let rec printList (l:'a list) =
   match l with
@@ -233,6 +438,8 @@ let rec printListListList (l:'a list) =
     print_char '\n';print_char '\n';
     printListListList tl
 
+(*==================================================MAJOR FUNCTION==============================================*)
+
 let parseStringForOperation (str : string) = 
   (*call all necessary function to do step 1 to 10 and have
   a converted list of chunk to do proper hash*)
@@ -246,7 +453,63 @@ let parseStringForOperation (str : string) =
                             (createZeroList (64 - (lenList (intToBinary (String.length str) [])))) (*step 7*)
                             (intToBinary (String.length str) [])))) (*step 6*)
 
+let hashFunction l = 
+  let newL = extendChunks l in (*step 11*)
+  let h0 = [0;1;1;0;0;1;1;1;0;1;0;0;0;1;0;1;0;0;1;0;0;0;1;1;0;0;0;0;0;0;0;1] in (*step 12*)
+  let h1 = [1;1;1;0;1;1;1;1;1;1;0;0;1;1;0;1;1;0;1;0;1;0;1;1;1;0;0;0;1;0;0;1] in
+  let h2 = [1;0;0;1;1;0;0;0;1;0;1;1;1;0;1;0;1;1;0;1;1;1;0;0;1;1;1;1;1;1;1;0] in
+  let h3 = [0;0;0;1;0;0;0;0;0;0;1;1;0;0;1;0;0;1;0;1;0;1;0;0;0;1;1;1;0;1;1;0] in
+  let h4 = [1;1;0;0;0;0;1;1;1;1;0;1;0;0;1;0;1;1;1;0;0;0;0;1;1;1;1;1;0;0;0;0] in
 
-let  x = printListListList (parseStringForOperation "42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test 42 test");      
+  let a = [0;1;1;0;0;1;1;1;0;1;0;0;0;1;0;1;0;0;1;0;0;0;1;1;0;0;0;0;0;0;0;1] in
+  let b = [1;1;1;0;1;1;1;1;1;1;0;0;1;1;0;1;1;0;1;0;1;0;1;1;1;0;0;0;1;0;0;1] in
+  let c = [1;0;0;1;1;0;0;0;1;0;1;1;1;0;1;0;1;1;0;1;1;1;0;0;1;1;1;1;1;1;1;0] in
+  let d = [0;0;0;1;0;0;0;0;0;0;1;1;0;0;1;0;0;1;0;1;0;1;0;0;0;1;1;1;0;1;1;0] in
+  let e = [1;1;0;0;0;0;1;1;1;1;0;1;0;0;1;0;1;1;1;0;0;0;0;1;1;1;1;1;0;0;0;0] in
+
+  chunksBitwiseOperation newL h0 h1 h2 h3 h4 a b c d e (*step 13*)
+
+let convertBinaryListToHexaString h0 h1 h2 h3 h4 =
+  let convert4Listtohexa nb =
+    match nb with
+    | 0 -> "0"
+    | 1 -> "1"
+    | 2 -> "2"
+    | 3 -> "3"
+    | 4 -> "4"
+    | 5 -> "5"
+    | 6 -> "6"
+    | 7 -> "7"
+    | 8 -> "8"
+    | 9 -> "9"
+    | 10 -> "a"
+    | 11 -> "b"
+    | 12 -> "c"
+    | 13 -> "d"
+    | 14 -> "e"
+    | 15 -> "f"
+    | e  -> "0" (*How did you get there *)
+  in
+  let rec convertBlockToHexa l =
+    match l with
+    | [] -> ""
+    | hd :: tl -> 
+      match hd with
+      | p1 :: p2 :: p3 :: p4 :: other -> (convert4Listtohexa (p4 + p3 * 2 + p2 * 4 + p1 * 8)) ^ convertBlockToHexa tl
+      | e -> "0" ^ convertBlockToHexa tl (*How did you get there again ?*)
+  in
+    let n0 = convertBlockToHexa (splitIntoBlockOf 4 h0) in (*step 14*)
+    let n1 = convertBlockToHexa (splitIntoBlockOf 4 h1) in
+    let n2 = convertBlockToHexa (splitIntoBlockOf 4 h2) in
+    let n3 = convertBlockToHexa (splitIntoBlockOf 4 h3) in
+    let n4 = convertBlockToHexa (splitIntoBlockOf 4 h4) in
+    n0 ^ n1 ^ n2 ^ n3 ^ n4 (*step 15*)
+
+
+let hash str =
+  let h0, h1, h2, h3, h4 = hashFunction (parseStringForOperation str) in
+    print_string (convertBinaryListToHexaString h0 h1 h2 h3 h4)
+
+let  x = hash teststring;      
         print_char '\n';print_char '\n'
 
