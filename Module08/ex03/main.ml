9aa1ff7e69d6646b9561862ad56e6c48d12ce779
Liam Flandrinck <lflandri@2B3.42angouleme.fr>
2025-12-15 14:06:08 +0100

    M08E03

--- Module08/ex03/main.ml
+++ Module08/ex03/main.ml
@@ -3,13 +3,12 @@
 (*                                                        :::      ::::::::   *)
 (*   main.ml                                            :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
-(*   By: Leka Uïla <liam.flandrinck.58@gmail.com    +#+  +:+       +#+        *)
+(*   By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2025/12/04 11:21:58 by Leka Uïla         #+#    #+#             *)
-(*   Updated: 2025/12/10 17:20:02 by Leka Uïla        ###   ########.fr       *)
+(*   Updated: 2025/12/15 14:05:26 by lflandri         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
-
 
 module Try =
     struct
@@ -28,16 +27,12 @@ module Try =
         }
       let bind (x:'a t) (f:('a -> 'b t)) : 'b t =
         let {content} = x in
-        let retValue = (f content) in
-        let {state} = retValue  in
-        let {error} = retValue  in
-        if state then
-          retValue
-        else
-          {
+        try
+          (f content)
+        with e ->           {
             content = content;
-            state   = state;
-            error   = error
+            state   = false;
+            error   = e
           }
       
       let recover (x:'a t) (f:exn -> 'a t) : 'a t = 
@@ -55,7 +50,7 @@ module Try =
             state   = false;
             error   = BadFilter "content doesn't satisfy the filter function"
           }
-      let flattenn (x:'a t t) : 'a t =
+      let flatten (x:'a t t) : 'a t =
         let {content} = x in
         let {state} = x in
         let {error} = x in
@@ -71,9 +66,64 @@ module Try =
           
     end
 
+
+
+(*=========================================================================================================================*)
+
+exception FakeError of string
+exception NotAFakeError of string
+
+
+let intfakefunction nb = 
+  if nb = 1 then
+    Try.return (nb + 2)
+  else
+    raise (FakeError "i don't want other than 1")
+
+let intfakefunctionRevover err = 
+  if err = (FakeError "i don't want other than 1") then
+    Try.return 1
+  else
+    Try.return 42
+
+let isOne nb = 
+  if nb = 1 then
+    true
+  else
+    false
+
+let printTry (x:'a Try.t) = 
+    let content = x.content in
+    let state = x.state in
+    let error = x.error in
+    print_int content;
+    print_string "  ";
+    if state then
+      print_string "true  "
+    else
+      print_string "false  ";
+    print_endline (Printexc.to_string error)
+
 let main () =
-  let i = Try.return 1 in print_endline "tset"
-
-
+  let i1 =  Try.return 1 in
+  let i2 =  Try.return 2 in
+  printTry i1;
+  printTry i2;
+  let i3 = Try.bind i1 intfakefunction in
+  let ei2 = Try.bind i2 intfakefunction in
+  printTry i3;
+  printTry ei2;
+  let i3 = Try.recover i3 intfakefunctionRevover in
+  let ei2 = Try.recover ei2 intfakefunctionRevover in
+  printTry i3;
+  printTry ei2;
+  let ei3 = Try.filter i3 isOne in
+  let i1 = Try.filter ei2 isOne in
+  printTry ei3;
+  printTry i1;
+  let t1 = Try.flatten (Try.return ei3) in
+  let t2 = Try.flatten (Try.return ei2) in
+  printTry t1;
+  printTry t2
 
 let () = main ()
